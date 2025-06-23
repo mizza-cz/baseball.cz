f414f5b3baeed093c7389d3231013e547dc76dd3
mizza-cz <mizza.cz666@gmail.com>
2025-06-23 09:07:00 +0200

    fix header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,53 +1,147 @@
-headerNavOpenerClick();
-function headerNavOpenerClick() {
-  const bodyEl = document.querySelector("body");
-  const headerNavOpener = document.querySelector(".js-header__opener");
-  if (!bodyEl || !headerNavOpener) {
-    return;
+(function ($) {
+  "use strict";
+
+  const DESKTOP_WIDTH = 1200;
+
+  function bindHamburger() {
+    const $body = $("body");
+    const $opener = $(".js-header__opener");
+
+    $opener.on("click", function (e) {
+      e.stopPropagation();
+      $opener.toggleClass("is-open");
+      $body.toggleClass("is-nav-open");
+    });
+
+    $(document).on("click", function (e) {
+      if (
+        $body.hasClass("is-nav-open") &&
+        !$(e.target).closest(".menu, .js-header__opener").length
+      ) {
+        $opener.removeClass("is-open");
+        $body.removeClass("is-nav-open");
+      }
+    });
   }
-  headerNavOpener.addEventListener("click", function () {
-    if (!this.classList.contains("is-open")) {
-      bodyEl.classList.add("is-nav-open");
-      this.classList.add("is-open");
-    } else {
-      bodyEl.classList.remove("is-nav-open");
-      this.classList.remove("is-open");
+
+  function bindSearchPopup() {
+    const SELECTORS = {
+      btn: ".header__search",
+      popup: "#mobile-search",
+
+      close:
+        ".js-search-close, .mobile-search__close,      \
+              .search-close, .btn-close, .close, .searchBox__close, \
+              [data-search-close]",
+    };
+
+    const $btn = $(SELECTORS.btn);
+    const $popup = $(SELECTORS.popup);
+
+    // открыть / закрыть по лупе
+    $btn.on("click", function (e) {
+      e.stopPropagation();
+      toggleSearch();
+    });
+
+    // закрыть по клику на любой крестик
+    $(document).on("click", SELECTORS.close, function (e) {
+      if ($popup.hasClass("active")) {
+        e.stopPropagation();
+        closeSearch();
+      }
+    });
+
+    $(document).on("click", function (e) {
+      if (
+        $popup.hasClass("active") &&
+        !$(e.target).closest(SELECTORS.popup + "," + SELECTORS.btn).length
+      ) {
+        closeSearch();
+      }
+    });
+
+    function toggleSearch() {
+      $btn.toggleClass("active");
+      $popup.toggleClass("active");
+      if ($popup.hasClass("active")) {
+        $popup
+          .find("input[type='search'], input[type='text']")
+          .first()
+          .trigger("focus");
+      }
     }
-  });
-}
-$(function () {
-  $(document).on("click", function (event) {
-    if (!$(event.target).closest(".navbar__menu-list").length) {
-      $(".navbar__menu-list").removeClass("active");
+    function closeSearch() {
+      $btn.removeClass("active");
+      $popup.removeClass("active");
     }
+  }
+
+  function bindMainMenu() {
+    const $menuItems = $(".navbar__menu-item");
+    const $menuLists = $(".navbar__menu-list");
+    let t;
+
+    function unbindAll() {
+      $menuItems.off("click");
+      $menuLists.off("mouseenter mouseleave");
+    }
+
+    function bindDesktop() {
+      $menuLists
+        .on("mouseenter", function () {
+          $(this).addClass("active");
+        })
+        .on("mouseleave", function () {
+          $(this).removeClass("active");
+        });
+    }
+
+    function bindMobile() {
+      $menuItems.on("click", function (e) {
+        const $cur = $(this).closest(".navbar__menu-list");
+        $menuLists.not($cur).removeClass("active");
+        $cur.toggleClass("active");
+        e.stopPropagation();
+      });
+    }
+
+    function refresh() {
+      unbindAll();
+      $(window).width() >= DESKTOP_WIDTH ? bindDesktop() : bindMobile();
+    }
+
+    refresh();
+    $(window).on("resize", function () {
+      clearTimeout(t);
+      t = setTimeout(refresh, 200);
+    });
+  }
+
+  function bindStickyHeader() {
+    const $header = $(".header");
+    let ticking = false;
+
+    function onScroll() {
+      if (!ticking) {
+        window.requestAnimationFrame(function () {
+          $("html,body").scrollTop() > 0
+            ? $header.addClass("header--scroll")
+            : $header.removeClass("header--scroll");
+          ticking = false;
+        });
+        ticking = true;
+      }
+    }
+
+    $(window).on("scroll", onScroll);
+    onScroll();
+  }
+
+  $(function () {
+    bindHamburger();
+    bindSearchPopup();
+    bindMainMenu();
+    bindStickyHeader();
   });
-
-  $(".navbar__menu-item").on("click", function (event) {
-    const $currentMenuList = $(this).closest(".navbar__menu-list");
-
-    $(".navbar__menu-list").not($currentMenuList).removeClass("active");
-
-    $currentMenuList.toggleClass("active");
-
-    event.stopPropagation();
-  });
-});
-
-// search
-const searchToggleBtn = document.querySelector(".header__search");
-const searchBox = document.getElementById("mobile-search");
-const closeBtn = searchBox?.querySelector(".searchBox__close");
-
-if (searchToggleBtn && searchBox) {
-  searchToggleBtn.addEventListener("click", function () {
-    searchBox.classList.toggle("active");
-    searchToggleBtn.classList.toggle("active");
-  });
-}
-
-if (closeBtn && searchToggleBtn && searchBox) {
-  closeBtn.addEventListener("click", function () {
-    searchBox.classList.remove("active");
-    searchToggleBtn.classList.remove("active");
-  });
-}
+})(jQuery);
